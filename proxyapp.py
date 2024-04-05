4052847eaaca0ee1928f5e99e0df85ed7e73a8e6
Psyxology <132628284+Psyxology@users.noreply.github.com>
2024-04-05 10:51:47 -0400

    Update proxyapp.py

--- proxyapp.py
+++ proxyapp.py
@@ -1,13 +1,21 @@
-from flask import Flask
-from requests import get
+from flask import Flask, request
+from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
+# Enable CORS for all domains on all routes
+CORS(app)
+
 SITE_NAME = 'https://d2hfhz0c37x28y.cloudfront.net/prod/stats?details=true'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
-  return get(f'{SITE_NAME}{path}').content
+    # Forward the original query parameters to the external service
+    response = requests.get(f'{SITE_NAME}{path}', params=request.args)
+    # Return the content of the response from the external service
+    # along with the correct content type and status code.
+    return (response.content, response.status_code, response.headers.items())
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
